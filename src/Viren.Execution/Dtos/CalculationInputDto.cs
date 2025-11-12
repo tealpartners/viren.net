9a0676d79540306441770443b958807a6853365e
Thomas Brouckaert <thomas@tealpartners.com>
2025-11-12 14:51:29 +0100

    Set Is Test on correct level

--- src/Viren.Execution/Dtos/CalculationInputDto.cs
+++ src/Viren.Execution/Dtos/CalculationInputDto.cs
@@ -11,7 +11,6 @@ namespace Viren.Execution.Dtos
         }
 
         public string RequestId { get; set; }
-        public bool IsTest { get; set; }
         public IDictionary<string, object> Root { get; set; }
         public IDictionary<string, object> Globals { get; set; }
     }
